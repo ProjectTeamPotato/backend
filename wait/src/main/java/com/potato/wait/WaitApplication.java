577b14b1ba1829bafd2f042f15449387ce11d0c9
kwonjinwoo <jinwkwon@smilegate.com>
2022-09-05 22:47:01 +0900

    init commit

---
+++ wait/src/main/java/com/potato/wait/WaitApplication.java
@@ -0,0 +1,13 @@
+package com.potato.wait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaitApplication.class, args);
+    }
+
+}
---
+++ wait/src/test/java/com/potato/wait/WaitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potato.wait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
