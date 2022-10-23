df8959af2c430e6d1cfd56b12bbdbe33c96c7748
lastvirgo266 <lastvirgo266@naver.com>
2022-10-23 21:07:50 +0900

    * start, stop 쉘스크립트 추가

---
+++ pay/start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+nohub java -jar pay.jar
---
+++ pay/stop.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+RUN_PID=$(lsof - ti tcp:443)
+
+if [ -z ${RUN_PID} ]
+then
+  echo "Not anything run"
+else
+  echo "Shutdown Spring boot Application"
+  kill -15 ${RUN_PID}
+fi
