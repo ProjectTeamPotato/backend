24eceb796cd3007ebcfcc01a2764e15f6c53c3fb
lastvirgo266 <lastvirgo266@gmail.com>
2022-11-19 18:36:15 +0900

    * Toss API 초안 작성   - Toss API 파악 및 초안 작성
    * Rest Template 설정 추가

---
+++ pay/src/main/java/com/potato/pay/api/toss/TossApiConstants.java
@@ -0,0 +1,19 @@
+package com.potato.pay.api.toss;
+
+public class TossApiConstants {
+
+    // 유틸리티 클래스는 생성되면 안됨
+    private TossApiConstants() {
+        throw new IllegalStateException("Constants Class");
+    }
+    private static final String TOSS = "https://api.tosspayments.com/";
+    private static final String V1 = "v1/";
+    private static final String NOW_VERSION = V1;
+    private static final String TOSS_API = TOSS + NOW_VERSION;
+
+
+    // -------------------- API LIST --------------------------- //
+
+    public static final  String CREATE_PAY = TOSS_API + "payments";
+
+}
