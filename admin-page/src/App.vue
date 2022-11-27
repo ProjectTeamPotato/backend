a56a488cf3d521f5ac399b2b59eb0d89128dc20d
kwonjinwoo <jinwkwon@smilegate.com>
2022-11-27 20:54:41 +0900

    [feat] admin page init

---
+++ admin-page/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <Login_Page/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Login_Page from './components/Login_Page.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    Login_Page,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
---
+++ admin-page/src/components/Login_Page.vue
@@ -0,0 +1,68 @@
+<template>
+    <v-container id="wrap">
+
+        <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+        >
+            <span id="title">
+                인터파크 클론코딩 관리자 페이지
+            </span>
+            <v-text-field
+            label="E-mail"
+            required
+            ></v-text-field>
+
+            <v-text-field
+            label="password"
+            required
+            ></v-text-field>
+
+            <v-btn
+            color="success"
+            class="mr-4"
+            @click="validate"
+            >
+            login
+            </v-btn>
+
+            <v-btn
+            color="error"
+            class="mr-4"
+            @click="reset"
+            >
+            Reset Form
+            </v-btn>
+        </v-form>
+    </v-container>
+</template>
+<style scoped>
+    #title {
+        display: flex;
+        justify-content: center;
+        margin: 40px;
+    }
+    #wrap {
+        display: flex;
+        justify-content: center;
+        align-items:center;
+        min-height: 100vh;
+    }
+    .v-form {
+        width: 1000px;
+    }
+</style>
+<script>
+export default {
+    setup() {
+        
+    },
+
+    methods: {
+        send_login() {
+            
+        }
+    }
+}
+</script>
