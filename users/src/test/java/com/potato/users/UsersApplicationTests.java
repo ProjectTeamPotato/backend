c3b77a7f8d00698f1932fdf5004f6a102df7a3b6
doxxx <doxxx93@gmail.com>
2023-01-27 00:35:23 +0900

    users init

---
+++ users/src/test/java/com/potato/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potato.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
